b57a88d339f4bd46f1d5128b2582bc52af03c7ff
sherif <sherifahmed852002@gmail.com>
2023-11-13 00:04:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 /* eslint-disable no-unused-vars */
-import './App.scss'
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Globalcontext from './components/context/Globalcontext'
-import i18n from './components/i18n';
-import Home from './pages/home/Home'
-
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Globalcontext from "./components/context/Globalcontext";
+import i18n from "./components/i18n";
+import Home from "./pages/home/Home";
+import { useEffect } from "react";
 
 function App() {
-
   return (
-    <>
-    <Globalcontext>
-       <Navbar />
-       <Router>
+    <Router>
+      <Globalcontext>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio-app" element={<Home />} />
+
         </Routes>
-       </Router>
-       </Globalcontext>
-    </>
-  )
+      </Globalcontext>
+    </Router>
+  );
 }
 
-export default App
+export default App;
